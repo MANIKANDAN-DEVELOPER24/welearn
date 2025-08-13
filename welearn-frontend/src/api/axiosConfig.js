d58e284cfb18438734b40ba90f3a09ea7c74986a
Manikandan <manikandan.work.it@gmail.com>
2025-08-13 17:47:24 +0530

    api changes

--- welearn-frontend/src/api/axiosConfig.js
+++ welearn-frontend/src/api/axiosConfig.js
@@ -1,12 +1,25 @@
 
+// import axios from 'axios';
+
+// const api = axios.create({
+// const API_BASE :"https://welearn-backend-g57w.onrender.com/api";
+
+//   withCredentials: true, // ✅ send cookies
+//   xsrfCookieName: 'csrftoken', // ✅ Django default
+//   xsrfHeaderName: 'X-CSRFToken',
+// });
+
+// export default api;
+
 import axios from 'axios';
 
+const API_BASE = "https://welearn-backend-g57w.onrender.com/api";
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: API_BASE,
   withCredentials: true, // ✅ send cookies
   xsrfCookieName: 'csrftoken', // ✅ Django default
   xsrfHeaderName: 'X-CSRFToken',
 });
 
 export default api;
-
